ea6d8b592ea62b19b1328c5795db24f946053973
Ian Garnett <designpxls@gmail.com>
2021-07-13 23:59:33 -0400

    disable button upon submit

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const jobTitle = document.querySelector('.jobTitle');
 const petName = document.querySelector('.petName');
 const colorInput = document.querySelector('.colorInput');
 const favNumber = document.querySelector('.numbers');
+const formButton = document.querySelector('#submitButton');
 
 //Container Divs
 const petContainer = document.querySelector('.petName-Container');
@@ -23,57 +24,49 @@ const hobby2Container = document.querySelector('.hobby2-Container');
 const jobContainer = document.querySelector('.jobTitle-Container');
 const favColorContainer = document.querySelector('.colorContainer');
 const favNumContainer = document.querySelector('.favNumber-Container');
+ 
 
+/* -------------------------------------------------------------*/
 
-
-
-
-
-
+/* 
+Person Constructor Function:
+Person Object to house 
+User Input Data that then can be 
+passed on to next function
+*/
 
 function Person() {
+  // Person Key-Value Profile 
   this.firstName = inputFirstNme.value;
   this.lastName = inputLastNme.value;
-
   this.hobby1 = hobby1.value;
   this.hobby2 = hobby2.value;
-
   this.jobTitle = jobTitle.value;
-
   this.favColor = colorInput.value;
-
   this.favNumber = favNumber.value;
-
   this.petName = petName.value;
-
   this.firstInitial = this.firstName[0];
   this.secondInitial = this.lastName[0];
 
+  /* Method takes element and inserts
+  data inside and returnsS Element */
   this.applyStyles = function(element, text) {
     var elementToPage = element;
-
     elementToPage.innerText = this[text];
-    // elementToPage.style.fontSize = fontSize;
-    // elementToPage.style.position = "absolute";
-    // elementToPage.style.top = `${randomNumber(-2, 80)}%`;
-    // elementToPage.style.left = `${randomNumber(2, 60)}%`;
-
-    
     return elementToPage;
   }
 
 }
 
-// create Initials & print on page
-// what we need to assign to all elements created
-// Size
-// Positioning
-// Color
+/* "Style Element" a Constructor Object
+"Saves newly created elements and copy/data to keys that
+can then be placed on the page  */
 function StyleElement() {
 
+  // binds the 'This' keyword to Person Object
   Person.call(this);
 
-
+  // Defines new keys and elements values
   this.firstInitialStyle = this.applyStyles(document.createElement('p'), ['firstInitial']);
   this.lastInitialStyle = this.applyStyles(document.createElement('p'), ['secondInitial']);
   this.hobby1Style = this.applyStyles(document.createElement('p'), ['hobby1']);
@@ -83,51 +76,33 @@ function StyleElement() {
   this.petNameStyle = this.applyStyles(document.createElement('p'), ['petName']);
   this.favNumStyle = this.applyStyles(document.createElement('h3'), ['favNumber']);
 
+  // Method appends elements to containers of your choice
   this.printToPage = function(element, parentContainer) {
     parentContainer.append(element);
   }
-
-  this.cloneElements = function() {
-    for (let i = 0; i < 3; i++) {
-      element[i];
-    }
-  }
-
 }
-
-function randomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 
 
 /* ------------------------------------------- */
 
+// Build Function
+
 function createProfile() {
-  // Create a new object called newPerson
-  let newPerson = new Person();
-  /* binding the 'person' object to LogName
-  This will allow the 'this.getFullName()' to run
-  Bind creates a copy of LogName
-  */
   let generateStyle = new StyleElement();
 
-  // generateStyle.cloneElements();
+  // Let's generate the styled elements to be placed on the page
   generateStyle.printToPage(generateStyle.firstInitialStyle, firstNameContainer);
   generateStyle.printToPage(generateStyle.lastInitialStyle, lastNameContainer);
-
   generateStyle.printToPage(generateStyle.hobby1Style, hobby1Container);
   generateStyle.printToPage(generateStyle.hobby2Style, hobby2Container);
-
   generateStyle.printToPage(generateStyle.jobStyle, jobContainer);
-
   generateStyle.printToPage(generateStyle.favColorStyle, favColorContainer);
-
   generateStyle.printToPage(generateStyle.petNameStyle, petContainer);
-
   generateStyle.printToPage(generateStyle.favNumStyle, favNumContainer);
+
+  /* Now let's disable the submit button
+  so it's not clicked "51" times :) */
+  formButton.disabled = true;
 };
 
 
