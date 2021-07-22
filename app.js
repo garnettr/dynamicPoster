facc1ce2a3457225e382ca861e0150641e7aba32
Ian Garnett <designpxls@gmail.com>
2021-07-22 08:23:41 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -30,53 +30,12 @@ const error = document.querySelector('.error');
 
 /* -------------------------------------------------------------*/
 
-/* 
-Person Constructor Function:
-Person Object to house 
-User Input Data that then can be 
-passed on to next function
-*/
 
-function Person() {
-  // Person Key-Value Profile 
-  this.firstName = inputFirstNme.value;
-  this.lastName = inputLastNme.value;
-  this.hobby1 = hobby1.value;
-  this.hobby2 = hobby2.value;
-  this.jobTitle = jobTitle.value;
-  this.favColor = colorInput.value;
-  this.favNumber = favNumber.value;
-  this.petName = petName.value;
-  this.firstInitial = this.firstName[0];
-  this.secondInitial = this.lastName[0];
-}
-
-
-function StyleElements() {
-
-  // Calls the Person Object inside this function
-  Person.call(this);
-
-  this.applyStyles(document.createElement('p'), ['firstInitial'], firstNameContainer);
-  this.applyStyles(document.createElement('p'), ['secondInitial'], lastNameContainer);
-  this.applyStyles(document.createElement('p'), ['hobby1'], hobby1Container);
-  this.applyStyles(document.createElement('p'), ['hobby2'], hobby2Container);
-  this.applyStyles(document.createElement('p'), ['jobTitle'], jobContainer);
-  this.applyStyles(document.createElement('p'), ['favColor'], favColorContainer);
-  this.applyStyles(document.createElement('p'), ['petName'], petContainer);
-  this.applyStyles(document.createElement('h3'), ['favNumber'], favNumContainer);
-}
-
-// Method takes an element, inserts data inside and renders element onto page
-StyleElements.prototype.applyStyles = function(element, text, parentContainer) {
+function applyStyles(element, text, parentContainer) {
   var elementToPage = element;
-  elementToPage.innerText = this[text];
+  elementToPage.innerText = text;
   parentContainer.append(elementToPage);
 }
-
-
-
-
 
 
 
@@ -86,9 +45,19 @@ StyleElements.prototype.applyStyles = function(element, text, parentContainer) {
 
 function createProfile() {
 
+
+
   if (inputFirstNme.value.length != 0 && inputLastNme.value.length != 0) {
-    // Let's generate the styled elements to be placed on the page
-    let generateStyle = new StyleElements();
+
+
+    applyStyles(document.createElement('p'), inputFirstNme.value[0], firstNameContainer);
+    applyStyles(document.createElement('p'), inputFirstNme.value[0], lastNameContainer);
+    applyStyles(document.createElement('p'), hobby1.value, hobby1Container);
+    applyStyles(document.createElement('p'), hobby2.value, hobby2Container);
+    applyStyles(document.createElement('p'), jobTitle.value, jobContainer);
+    applyStyles(document.createElement('p'), colorInput.value, favColorContainer);
+    applyStyles(document.createElement('p'), petName.value, petContainer);
+    applyStyles(document.createElement('h3'), favNumber.value, favNumContainer);
 
     /* Now let's disable the submit button
     so it's not clicked "51" times :) */
